65184d23a4966383b84ee79c6938f8452b7ac7e1
Danny Nguyen <nguyeda3@oregonstate.edu>
2026-06-01 10:11:12 -0700

    Added helios and maru

--- Uma Countdown/src/App.jsx
+++ Uma Countdown/src/App.jsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
 import clocks from "./images/clocks.jpeg"
 import sadkita from "./images/sadkita.png"
+import helios from "./images/helios.png"
 
 
 const dates = {
   GrandLive: "2026-07-24T22:00:00Z",
+  Maruzensky: "2026-07-01T22:00:00Z",
+  DaitakuHelios:"2026-11-01T22:00:00Z"
+};
+
+const events = {
+  GrandLive: "Grand Live",
+  Maruzensky: "Maruzensky and Nakayama Festa",
+  DaitakuHelios: "Daitaku Helios"
 };
 
 export default function Countdown() {
-  const [target, setTarget] = useState(dates.GrandLive);
-  const [remaining, setRemaining] = useState("");
-
-  const eventDate = new Date("2026-07-24T22:00:00Z");
-
+  const [target, setTarget] = useState("GrandLive");  const [remaining, setRemaining] = useState("");
+  const eventDate = new Date(dates[target]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date();
-      const end = new Date(target);
-
+      const end = new Date(dates[target]);
       const diff = end - now;
 
       if (diff <= 0) {
@@ -47,23 +52,35 @@ export default function Countdown() {
 
   return (
     <div>
-      <select
-        value={target}
-        onChange={(e) => setTarget(e.target.value)}
-      >
-        {Object.entries(dates).map(([name, date]) => (
-          <option key={name} value={date}>
-            {name}
-          </option>
-        ))}
-      </select>
+    <select
+      value={target}
+      onChange={(e) => setTarget(e.target.value)}
+    >
+      {Object.keys(dates).map((name) => (
+        <option key={name} value={name}>
+          {events[name]}
+        </option>
+      ))}
+    </select>
 
       <h1>{remaining}</h1>
-      <p>
-        Grand Live starts at: {eventDate.toLocaleString()}
-      </p>
-      <img src={clocks} height="400px"/>
-      <img style={{paddingLeft: "100px"}}  src={sadkita} height="400px"/>
+    <p>
+      {events[target]} starts at: {eventDate.toLocaleString()}
+    </p>
+
+    {target == "GrandLive" && 
+      <div>
+        <img src={clocks} height="400px"/>
+        <img style={{paddingLeft: "100px"}}  src={sadkita} height="400px"/>
+      </div>
+    }
+
+    {target == "DaitakuHelios" && 
+      <div>
+        <img src={helios} height="400px"/>
+      </div>
+    }
+
     </div>
   );
 }
